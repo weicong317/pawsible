9c6bf93ede52946ec62a46d227b9e4b4c549fbe4
Unknown <weicong317@gmail.com>
2018-07-25 09:54:32 +0800

    created all tables needed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_110013) do
+ActiveRecord::Schema.define(version: 2018_07_25_015056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "challenges", force: :cascade do |t|
+    t.integer "category"
+    t.string "title"
+    t.text "description"
+    t.integer "difficulty"
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "dog_name"
+    t.string "breed"
+    t.text "biography"
+    t.json "profile_pic"
+    t.integer "status"
+    t.integer "total_points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.bigint "pet_id"
+    t.bigint "challenge_id"
+    t.json "video"
+    t.boolean "verification"
+    t.integer "number_of_rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["challenge_id"], name: "index_uploads_on_challenge_id"
+    t.index ["pet_id"], name: "index_uploads_on_pet_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_110013) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
