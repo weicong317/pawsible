d45983edc96dfdfd37c351ec904bebf61b94e8a8
Unknown <weicong317@gmail.com>
2018-07-25 10:10:27 +0800

    added validations to model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,8 @@
 class Challenge < ApplicationRecord
+  has_many :uploads
+
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :point, presence: true
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,8 @@
 class Pet < ApplicationRecord
+  belongs_to :user
+  has_many :uploads
+
+  validates :user_id, presence: true
+  validates :dog_name, presence: true
+  validates :breed, presence: true
 end
