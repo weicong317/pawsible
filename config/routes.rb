0cc65d2a556764341ef01d3dcb1f4a0e96ae9343
Unknown <weicong317@gmail.com>
2018-07-26 11:33:34 +0800

    edit profile and added edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :edit, :update]
 
   # from clearance
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
