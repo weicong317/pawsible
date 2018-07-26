4f411a254b0bad75b9decebe16044cebdcb020ca
Unknown <weicong317@gmail.com>
2018-07-26 14:41:56 +0800

    added carrierwave to upload video from challenge

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,6 +2,8 @@ class Upload < ApplicationRecord
   belongs_to :user
   belongs_to :challenge
 
+  mount_uploader :media, MediaUploader
+
   # validates :user_id, presence: true
   # validates :challenge_id, presence: true
   # validates :media, presence: true
