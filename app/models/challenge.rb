7e0287290386137c9c6aa046c9f05eb36385883d
Unknown <weicong317@gmail.com>
2018-07-30 15:45:34 +0800

    remove byebug

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,8 @@
 class Challenge < ApplicationRecord
   has_many :uploads, dependent: :destroy
 
+  scope :starts_with, -> (challenge_title) { where("title ILIKE ?", "#{challenge_title}%")}
+
   # validates :sponsor, presence: true
   # validates :title, presence: true
   # validates :description, presence: true
